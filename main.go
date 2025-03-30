13f3858569262f77c2a7477e4659383d24d41952
Enis Şimşir <en.simsir@gmail.com>
2025-03-31 01:47:32 +0300

    websocket + gorilla

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handleConnections(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("WebSocket connection upgrade failed:", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Connection lost:", err)
+			break
+		}
+		fmt.Printf("Message taken: %s\n", msg)
+
+		// Gelen mesajı geri gönder
+		err = conn.WriteMessage(websocket.TextMessage, msg)
+		if err != nil {
+			fmt.Println("Error sending message:", err)
+			break
+		}
+	}
+}
+
+func main() {
+	http.HandleFunc("/ws", handleConnections)
+	fmt.Println("WebSocket server working on 8080 port...")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Server cannot start:", err)
+	}
+}
