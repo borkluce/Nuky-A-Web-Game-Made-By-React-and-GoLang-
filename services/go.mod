b5edac80e55d8857467e9419f6f86239ae7fcfb6
Enis Şimşir <en.simsir@gmail.com>
2025-04-03 01:07:56 +0300

    noluyo aq

--- services/go.mod
+++ services/go.mod
@@ -5,6 +5,12 @@ go 1.24.1
 require github.com/gofiber/fiber/v2 v2.52.6
 
 require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
+
+require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
@@ -17,4 +23,5 @@ require (
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
+	gorm.io/gorm v1.25.12
 )
--- services/internal/auth/repo/user_repo.go
+++ services/internal/auth/repo/user_repo.go
@@ -1,14 +1,33 @@
 package repo
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
 
 type AuthRepo struct {
+	db *gorm.DB
 }
 
-func NewAuthRepo() *AuthRepo {
-	return &AuthRepo{}
+func NewAuthRepo(db *gorm.DB) *AuthRepo {
+	return &AuthRepo{db: db}
 }
 
-func (ap *AuthRepo) CreateUser(ctx *fiber.Ctx, user *model.User) {}
+func (ap *AuthRepo) CreateUser(user *models.User) error {
+	if err := ap.db.Create(user).Error; err != nil {
+		return err
+	}
+	return nil
+}
 
-func (ap *AuthRepo) GetUser(ctx *fiber.Ctx, userID int8) (*model.User, error) {}
+func (ap *AuthRepo) GetUserByEmail(email string) (*models.User, error) {
+	var user models.User
+	if err := ap.db.Where("email = ?", email).First(&user).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errors.New("user not found")
+		}
+		return nil, err
+	}
+	return &user, nil
+}
