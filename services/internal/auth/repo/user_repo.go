e7b7c798ca341058c707ceda7433f552029fd148
enissimsir <en.simsir@gmail.com>
2025-04-03 17:15:06 +0300

    deleted files

--- services/internal/auth/repo/user_repo.go
+++ services/internal/auth/repo/user_repo.go
@@ -4,6 +4,8 @@ import (
 	"errors"
 
 	"gorm.io/gorm"
+
+	"services/internal/auth/model"
 )
 
 type AuthRepo struct {
@@ -14,15 +16,15 @@ func NewAuthRepo(db *gorm.DB) *AuthRepo {
 	return &AuthRepo{db: db}
 }
 
-func (ap *AuthRepo) CreateUser(user *models.User) error {
+func (ap *AuthRepo) CreateUser(user *model.User) error {
 	if err := ap.db.Create(user).Error; err != nil {
 		return err
 	}
 	return nil
 }
 
-func (ap *AuthRepo) GetUserByEmail(email string) (*models.User, error) {
-	var user models.User
+func (ap *AuthRepo) GetUserByEmail(email string) (*model.User, error) {
+	var user model.User
 	if err := ap.db.Where("email = ?", email).First(&user).Error; err != nil {
 		if errors.Is(err, gorm.ErrRecordNotFound) {
 			return nil, errors.New("user not found")
@@ -30,4 +32,22 @@ func (ap *AuthRepo) GetUserByEmail(email string) (*models.User, error) {
 		return nil, err
 	}
 	return &user, nil
+}
+
+func (ap *AuthRepo) GetUserByID(userID int) (*model.User, error) {
+	var user model.User
+	if err := ap.db.First(&user, userID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, errors.New("user not found")
+		}
+		return nil, err
+	}
+	return &user, nil
+}
+
+func (ap *AuthRepo) UpdateLastMoveDate(userID int, lastMoveDate string) error {
+	if err := ap.db.Model(&model.User{}).Where("id = ?", userID).Update("last_move_date", lastMoveDate).Error; err != nil {
+		return err
+	}
+	return nil
 }
