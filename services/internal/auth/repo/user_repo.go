82438ad7c3a5bb87f8a0532891c5b62c0ec896b2
enissimsir <en.simsir@gmail.com>
2025-04-22 16:30:32 +0300

    auth pkg imported

--- services/internal/auth/repo/user_repo.go
+++ services/internal/auth/repo/user_repo.go
@@ -4,9 +4,9 @@ import (
 	"context"
 	"services/internal/auth/model"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/bson"
 )
 
 type UserRepo struct {
