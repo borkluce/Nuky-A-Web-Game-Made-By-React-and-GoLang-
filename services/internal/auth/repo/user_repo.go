a0acc3e56404c7143c7f815dd3e242f796ed8045
enissimsir <en.simsir@gmail.com>
2025-05-11 01:40:03 +0300

    user_repo.go update

--- services/internal/auth/repo/user_repo.go
+++ services/internal/auth/repo/user_repo.go
@@ -13,9 +13,9 @@ type UserRepo struct {
 	collection *mongo.Collection
 }
 
-func NewUserRepo(db *mongo.Database) *UserRepo {
+func NewUserRepo(collection *mongo.Collection) *UserRepo {
 	return &UserRepo{
-		collection: db.Collection("users"),
+		collection: collection,
 	}
 }
 
@@ -31,7 +31,7 @@ func (ur *UserRepo) GetUserByID(ctx context.Context, id primitive.ObjectID) (*mo
 }
 
 func (ur *UserRepo) PutUser(ctx context.Context, user model.User) error {
-	filter := bson.M{"_id", user.ID}
+	filter := bson.M{"_id": user.ID}
 	update := bson.M{
 		"$set": bson.M{
 			"email":        user.Email,
@@ -39,4 +39,46 @@ func (ur *UserRepo) PutUser(ctx context.Context, user model.User) error {
 			"lastMoveDate": user.LastMoveDate,
 		},
 	}
+
+	_, err := ur.collection.UpdateOne(ctx, filter, update)
+	return err
+}
+
+func (ur *UserRepo) CreateUser(ctx context.Context, user model.User) (primitive.ObjectID, error) {
+	if user.ID.IsZero() {
+		user.ID = primitive.NewObjectID()
+	}
+
+	result, err := ur.collection.InsertOne(ctx, user)
+	if err != nil {
+		return primitive.NilObjectID, err
+	}
+
+	if oid, ok := result.InsertedID.(primitive.ObjectID); ok {
+		return oid, nil
+	}
+
+	return primitive.NilObjectID, nil
+}
+
+func (ur *UserRepo) GetUserByEmail(ctx context.Context, email string) (*model.User, error) {
+	var user model.User
+	filter := bson.M{"email": email}
+
+	if err := ur.collection.FindOne(ctx, filter).Decode(&user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (ur *UserRepo) GetUserByUsername(ctx context.Context, username string) (*model.User, error) {
+	var user model.User
+	filter := bson.M{"username": username}
+
+	if err := ur.collection.FindOne(ctx, filter).Decode(&user); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
 }
